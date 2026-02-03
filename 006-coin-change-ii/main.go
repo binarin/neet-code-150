f43f0d3ce7299190298804a5742b7d9ea0b57c03
Alexey Lebedeff <binarin@binarin.info>
2026-02-03 17:47:06 +0100

    006-coin-change-ii - add memoization on top of the recursive solution
    is this a DP? :)

--- 006-coin-change-ii/main.go
+++ 006-coin-change-ii/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println(change(5, []int{1, 2, 5}))
 }
 
 func changeBf(amount int, coins []int) int {
@@ -22,4 +22,39 @@ func changeBf(amount int, coins []int) int {
 	}
 
 	return changeBf(amount-coins[0], coins) + changeBf(amount, coins[1:])
+}
+
+func change(amount int, coins []int) int {
+	// we are using the fact that amounts are unique, so using the
+	// first available coin as a cache key is a valid approach.
+	type CacheKey struct {
+		a int
+		f int
+	}
+	cache := map[CacheKey]int{}
+
+	var recur func(int, []int) int
+	recur = func(amount_r int, coins_r []int) int {
+		if amount_r < 0 {
+			return 0
+		}
+		if len(coins_r) == 0 {
+			return 0
+		}
+
+		cache_key := CacheKey{a: amount_r, f: coins_r[0]}
+		if value, is_cached := cache[cache_key]; is_cached {
+			return value
+		}
+		var result int
+		if amount_r == 0 {
+			result = 1
+		} else {
+			result = recur(amount_r-coins_r[0], coins_r) + recur(amount_r, coins_r[1:])
+		}
+		cache[cache_key] = result
+		return result
+	}
+
+	return recur(amount, coins)
 }
--- 006-coin-change-ii/main_test.go
+++ 006-coin-change-ii/main_test.go
@@ -7,6 +7,12 @@ import (
 )
 
 func TestChange(t *testing.T) {
+	assert.Equal(t, 4, change(5, []int{1, 2, 5}))
+	assert.Equal(t, 0, change(3, []int{2}))
+	assert.Equal(t, 1, change(10, []int{10}))
+}
+
+func TestChangeBf(t *testing.T) {
 	assert.Equal(t, 4, changeBf(5, []int{1, 2, 5}))
 	assert.Equal(t, 0, changeBf(3, []int{2}))
 	assert.Equal(t, 1, changeBf(10, []int{10}))
