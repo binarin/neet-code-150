8c84310d57e0e690952f04f3b24eadc001d0cdc9
Alexey Lebedeff <binarin@binarin.info>
2026-02-04 15:31:16 +0100

    3sum - quadratic

--- 012-3sum/main.go
+++ 012-3sum/main.go
@@ -10,22 +10,43 @@ func main() {
 }
 
 func threeSum(nums []int) [][]int {
-	triplets := map[[3]int]bool{}
 	if len(nums) < 3 {
 		return [][]int{}
 	}
-	for i := 0; i < len(nums)-2; i++ {
-		for j := i + 1; j < len(nums)-1; j++ {
-			for k := j + 1; k < len(nums); k++ {
-				if nums[i]+nums[j]+nums[k] == 0 {
-					triplet_slice := []int{nums[i], nums[j], nums[k]}
-					slices.Sort(triplet_slice)
-					triplet_array := [3]int{triplet_slice[0], triplet_slice[1], triplet_slice[2]}
-					triplets[triplet_array] = true
+
+	lookup := map[int][]int{}
+	for i, n := range nums {
+		lookup[n] = append(lookup[n], i)
+	}
+
+	// 2 elements are enough to identify triplet
+	triplets := map[[3]int]bool{}
+
+	pack := func(i int, j int, k int) [3]int {
+		as_slice := []int{nums[i], nums[j], nums[k]}
+		slices.Sort(as_slice)
+		return [3]int{as_slice[0], as_slice[1], as_slice[2]}
+	}
+
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			third_number := -(nums[i] + nums[j])
+			found := false
+			var k int
+			for _, nums_idx := range lookup[third_number] {
+				if nums_idx == i || nums_idx == j {
+					continue
 				}
+				found = true
+				k = nums_idx
+				break
+			}
+			if found {
+				triplets[pack(i, j, k)] = true
 			}
 		}
 	}
+
 	result := [][]int{}
 	for k := range triplets {
 		result = append(result, k[:])
