710f47bea95efebeb0851765436f0a598227597f
Alexey Lebedeff <binarin@binarin.info>
2026-02-04 10:16:36 +0100

    container-with-most-water - 2-pointer solution

--- 010-container-with-most-water/main.go
+++ 010-container-with-most-water/main.go
@@ -12,6 +12,25 @@ func maxArea(height []int) int {
 	if len(height) < 2 {
 		return 0
 	}
+	left := 0
+	right := len(height) - 1
+	max_area := 0
+	for left < right {
+		area := min(height[left], height[right]) * (right - left)
+		max_area = max(max_area, area)
+		if height[left] < height[right] { // the smaller can never improve situation
+			left++
+		} else {
+			right--
+		}
+	}
+	return max_area
+}
+
+func maxAreaBf(height []int) int {
+	if len(height) < 2 {
+		return 0
+	}
 	max_area := 0
 	for i := 0; i < len(height)-1; i++ {
 		for j := i + 1; j < len(height); j++ {
