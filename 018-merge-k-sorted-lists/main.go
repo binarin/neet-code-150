39751c81704d069de77f3ef0fee6a2787773ea35
Alexey Lebedeff <binarin@binarin.info>
2026-02-05 09:36:17 +0100

    merge-k-sorted-lists - merge 2 lists using iterative approach instead of recursive one

--- 018-merge-k-sorted-lists/main.go
+++ 018-merge-k-sorted-lists/main.go
@@ -51,6 +51,43 @@ func mergeLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	return l1
 }
 
+func mergeListsIter(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	var head, cur *ListNode
+	if l2.Val < l1.Val {
+		l1, l2 = l2, l1
+	}
+	head = l1
+	cur = head
+	l1 = l1.Next
+
+	for {
+		if l1 == nil {
+			cur.Next = l2
+			break
+		}
+		if l2 == nil {
+			cur.Next = l1
+			break
+		}
+
+		if l2.Val < l1.Val {
+			l1, l2 = l2, l1
+		}
+		cur.Next = l1
+		cur = l1
+		l1 = l1.Next
+	}
+	return head
+
+}
+
 func mergeKLists(lists []*ListNode) *ListNode {
 	if len(lists) == 0 {
 		return nil
@@ -61,5 +98,5 @@ func mergeKLists(lists []*ListNode) *ListNode {
 	// ----------- -----------
 	left := mergeKLists(lists[0 : len(lists)/2])
 	right := mergeKLists(lists[len(lists)/2:])
-	return mergeLists(left, right)
+	return mergeListsIter(left, right)
 }
