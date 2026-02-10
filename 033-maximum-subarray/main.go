95d080504ddd2b7ed896b04d01040c8dc2231f20
Alexey Lebedeff <binarin@binarin.info>
2026-02-10 15:53:58 +0100

    maximum-subarray - bruteforce solution

---
+++ 033-maximum-subarray/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+}
+
+func maxSubArray(nums []int) int {
+	result := math.MinInt
+	for i := 0; i < len(nums); i++ {
+		sum := 0
+		for j := i; j < len(nums); j++ {
+			sum += nums[j]
+			result = max(result, sum)
+		}
+	}
+	return result
+}
