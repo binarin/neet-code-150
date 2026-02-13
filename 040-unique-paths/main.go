782e9cf9fba5477b95b22ed2e159767921a2d5e8
Alexey Lebedeff <binarin@binarin.info>
2026-02-13 13:44:53 +0100

    unique-paths - add memoization

--- 040-unique-paths/main.go
+++ 040-unique-paths/main.go
@@ -9,8 +9,20 @@ func main() {
 }
 
 func uniquePaths(m int, n int) int {
-	if m == 1 || n == 1 {
-		return 1
+	memo := [][]int{}
+	for range m {
+		memo = append(memo, make([]int, n))
 	}
-	return uniquePaths(m, n-1) + uniquePaths(m-1, n)
+	var recur func(int, int) int
+	recur = func(m, n int) int {
+		if m == 1 || n == 1 {
+			return 1
+		}
+		if already := memo[m-1][n-1]; already == 0 {
+			memo[m-1][n-1] = recur(m, n-1) + recur(m-1, n)
+		}
+		return memo[m-1][n-1]
+	}
+
+	return recur(m, n)
 }
