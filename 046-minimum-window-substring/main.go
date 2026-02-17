2745a65f635a0f4441ecd884ed8928f4cbbe33cd
Alexey Lebedeff <binarin@binarin.info>
2026-02-17 17:48:27 +0100

    minimum-window-substring - linear solution

--- 046-minimum-window-substring/main.go
+++ 046-minimum-window-substring/main.go
@@ -2,94 +2,113 @@ package main
 
 import (
 	"fmt"
+	"maps"
 	"math"
+	"strings"
 )
 
 func main() {
-	fmt.Println(minWindow("cabwefgewcwaefgcf", "cae"))
+	fmt.Println(minWindow("a", "a"))
+	// fmt.Println(minWindow("cabwefgewcwaefgcf", "cae"))
 	// fmt.Println(minWindow("ADOBECODEBANC", "ABC"))
 }
 
+func use(...any) {
+}
+
 func minWindow(s string, t string) string {
-	charIdx := func(v byte) byte {
-		if 'a' <= v && v <= 'z' {
-			return v - 'a'
-		}
-		if 'A' <= v && v <= 'Z' {
-			return v - 'A' + 26
-		}
-		panic("Out of range char")
-	}
-	type Window [52]byte
-
-	current := Window{}
-	for i := 0; i < len(s); i++ {
-		current[charIdx(s[i])]++
+	if len(s) < len(t) || len(s) == 0 || len(t) == 0 {
+		return ""
 	}
 
-	target, target_length := Window{}, len(t)
-	for i := range target_length {
-		c_idx := charIdx(t[i])
-		target[c_idx]++
-		if target[c_idx] > current[c_idx] {
-			return ""
+	targetFreq := map[byte]int{}
+	targetLen := len(t)
+	for i := range targetLen {
+		targetFreq[t[i]]++
+	}
+
+	shortestLen, shortestWindow := math.MaxInt, ""
+
+	// invariant: chopLeft + currentLen + chopRight = haystackLen
+	chopLeft, chopRight, haystackLen := 0, len(s), len(s)
+
+	extra := map[byte]int{}
+
+	freq_fmt := func(f map[byte]int) string {
+		result := []string{}
+		for k, v := range f {
+			result = append(result, fmt.Sprintf("%c:%d", k, v))
+		}
+		return strings.Join(result, ",")
+	}
+	use(freq_fmt)
+
+	visualize := func(prefix string) {
+		// fmt.Printf("%s: %s→%s←%s (extra: %s)\n", prefix, s[:chopLeft], s[chopLeft:haystackLen-chopRight], s[haystackLen-chopRight:], freq_fmt(extra))
+	}
+
+	needed := maps.Clone(targetFreq)
+
+	extendRight := func() bool {
+		for len(needed) > 0 && chopRight > 0 {
+			// chopLeft + currentLen = chopLeft + haystackLen - chopLeft - chopRight = haystackLen - chopRight
+			addCharIdx := haystackLen - chopRight
+			chopRight--
+			if _, ok := targetFreq[s[addCharIdx]]; ok {
+				if _, ok := needed[s[addCharIdx]]; ok {
+					needed[s[addCharIdx]]--
+					if needed[s[addCharIdx]] == 0 {
+						delete(needed, s[addCharIdx])
+					}
+				} else {
+					extra[s[addCharIdx]]++
+				}
+			}
+		}
+		return len(needed) == 0
+	}
+
+	trimLeft := func() bool {
+		for chopLeft+targetLen <= haystackLen {
+			dropCharIdx := chopLeft
+			if _, ok := targetFreq[s[dropCharIdx]]; ok {
+				if val, ok := extra[s[dropCharIdx]]; ok {
+					if val > 1 {
+						extra[s[dropCharIdx]]--
+					} else {
+						delete(extra, s[dropCharIdx])
+					}
+				} else {
+					return true
+				}
+			}
+			chopLeft++
+
+		}
+		return false
+	}
+
+	maybeImprove := func() {
+		currentLen := haystackLen - chopLeft - chopRight
+		if currentLen < shortestLen {
+			shortestLen, shortestWindow = currentLen, s[chopLeft:haystackLen-chopRight]
 		}
 	}
 
-	shortest_len, shortest_window := math.MaxInt, ""
-
-	// _dump_win := func(w Window) []string {
-	// 	result := []string{}
-	// 	for idx, count := range target {
-	// 		if count > 0 {
-	// 			var val byte
-	// 			if idx < 26 {
-	// 				val = byte(idx) + 'a'
-	// 			} else {
-	// 				val = byte(idx) + 'A'
-	// 			}
-	// 			result = append(result, fmt.Sprintf("%c: %d", val, w[idx]))
-	// 		}
-	// 	}
-	// 	return result
-	// }
-
-	var recur func(int, int, Window)
-	recur = func(left, right int, current Window) {
-		for target[charIdx(s[left])] == 0 && right-left+1 >= target_length {
-			left++
+	for {
+		visualize("LO")
+		if !extendRight() {
+			break
 		}
-		for target[charIdx(s[right])] == 0 && right-left+1 >= target_length {
-			right--
+		visualize("EX")
+		if !trimLeft() {
+			break
 		}
-
-		substr := s[left : right+1]
-		current_len := right - left + 1
-		// fmt.Println(left, right, dump_win(current), shortest_len, shortest_window, current_len, substr)
-
-		if current_len < target_length {
-			panic("shouldn't be here")
-		}
-
-		if current_len < shortest_len {
-			shortest_len, shortest_window = current_len, substr
-		}
-
-		left_char_idx := charIdx(s[left])
-		if current[left_char_idx] > target[left_char_idx] {
-			sub := current
-			sub[left_char_idx]--
-			recur(left+1, right, sub)
-		}
-
-		right_char_idx := charIdx(s[right])
-		if current[right_char_idx] > target[right_char_idx] {
-			sub := current
-			sub[right_char_idx]--
-			recur(left, right-1, sub)
-		}
+		visualize("TR")
+		maybeImprove()
+		needed[s[chopLeft]]++
+		chopLeft++
 	}
 
-	recur(0, len(s)-1, current)
-	return shortest_window
+	return shortestWindow
 }
