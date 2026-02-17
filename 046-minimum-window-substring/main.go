12d76840f16a162f55702b80bc6cc632ce611a8f
Alexey Lebedeff <binarin@binarin.info>
2026-02-17 12:13:11 +0100

    minimum-window-substring - better, but not fast enough

--- 046-minimum-window-substring/main.go
+++ 046-minimum-window-substring/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println(minWindow("ab", "A"))
+	fmt.Println(minWindow("cabwefgewcwaefgcf", "cae"))
 	// fmt.Println(minWindow("ADOBECODEBANC", "ABC"))
 }
 
@@ -19,33 +20,76 @@ func minWindow(s string, t string) string {
 		}
 		panic("Out of range char")
 	}
-	count := func(s string) [52]byte {
-		result := [52]byte{}
-		for i := 0; i < len(s); i++ {
-			result[charIdx(s[i])]++
-		}
-		return result
+	type Window [52]byte
+
+	current := Window{}
+	for i := 0; i < len(s); i++ {
+		current[charIdx(s[i])]++
 	}
-	target := count(t)
-	enough := func(ct [52]byte) bool {
-		for i, v := range ct {
-			if target[i] > v {
-				return false
-			}
-		}
-		return true
-	}
-	shortest := s + "!"
-	for i := 0; i <= len(s)-len(t); i++ {
-		for j := i + len(t); j <= len(s); j++ {
-			substr := s[i:j]
-			if enough(count(substr)) && len(substr) < len(shortest) {
-				shortest = substr
-			}
+
+	target, target_length := Window{}, len(t)
+	for i := range target_length {
+		c_idx := charIdx(t[i])
+		target[c_idx]++
+		if target[c_idx] > current[c_idx] {
+			return ""
 		}
 	}
-	if len(shortest) > len(s) {
-		return ""
+
+	shortest_len, shortest_window := math.MaxInt, ""
+
+	// _dump_win := func(w Window) []string {
+	// 	result := []string{}
+	// 	for idx, count := range target {
+	// 		if count > 0 {
+	// 			var val byte
+	// 			if idx < 26 {
+	// 				val = byte(idx) + 'a'
+	// 			} else {
+	// 				val = byte(idx) + 'A'
+	// 			}
+	// 			result = append(result, fmt.Sprintf("%c: %d", val, w[idx]))
+	// 		}
+	// 	}
+	// 	return result
+	// }
+
+	var recur func(int, int, Window)
+	recur = func(left, right int, current Window) {
+		for target[charIdx(s[left])] == 0 && right-left+1 >= target_length {
+			left++
+		}
+		for target[charIdx(s[right])] == 0 && right-left+1 >= target_length {
+			right--
+		}
+
+		substr := s[left : right+1]
+		current_len := right - left + 1
+		// fmt.Println(left, right, dump_win(current), shortest_len, shortest_window, current_len, substr)
+
+		if current_len < target_length {
+			panic("shouldn't be here")
+		}
+
+		if current_len < shortest_len {
+			shortest_len, shortest_window = current_len, substr
+		}
+
+		left_char_idx := charIdx(s[left])
+		if current[left_char_idx] > target[left_char_idx] {
+			sub := current
+			sub[left_char_idx]--
+			recur(left+1, right, sub)
+		}
+
+		right_char_idx := charIdx(s[right])
+		if current[right_char_idx] > target[right_char_idx] {
+			sub := current
+			sub[right_char_idx]--
+			recur(left, right-1, sub)
+		}
 	}
-	return shortest
+
+	recur(0, len(s)-1, current)
+	return shortest_window
 }
--- 046-minimum-window-substring/main_test.go
+++ 046-minimum-window-substring/main_test.go
@@ -10,4 +10,6 @@ func TestMinWindow(t *testing.T) {
 	assert.Equal(t, "BANC", minWindow("ADOBECODEBANC", "ABC"))
 	assert.Equal(t, "a", minWindow("a", "a"))
 	assert.Equal(t, "", minWindow("a", "aa"))
+	assert.Equal(t, "", minWindow("ab", "A"))
+	assert.Equal(t, "cwae", minWindow("cabwefgewcwaefgcf", "cae"))
 }
