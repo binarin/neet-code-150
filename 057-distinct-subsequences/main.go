40a76c6321d2589e0a4d03db3172f97082a496db
Alexey Lebedeff <binarin@binarin.info>
2026-02-27 07:12:22 +0100

    distinct-subsequences - space-efficient dp

--- 057-distinct-subsequences/main.go
+++ 057-distinct-subsequences/main.go
@@ -8,7 +8,30 @@ func main() {
 	fmt.Println(numDistinct("rabbbit", "rabbit"))
 }
 
-func numDistinct(s string, t string) int {
+func numDistinct(s, t string) int {
+	return numDistinctDyn(s, t)
+}
+
+func numDistinctDyn(s string, t string) int {
+	cache := make([]int, len(t)+1)
+	cache[len(t)] = 1
+	for sidx := len(s) - 1; sidx >= 0; sidx-- {
+		tidxStart := min(len(t)-1, sidx)
+		diagCarry := cache[tidxStart+1]
+		// fmt.Printf("S: %d - %v\n", sidx, cache[:tidxStart+2])
+		for tidx := tidxStart; tidx >= 0; tidx-- {
+			thisCount := cache[tidx]
+			if s[sidx] == t[tidx] {
+				thisCount += diagCarry
+			}
+			// fmt.Printf("tidx %d, old cache %d, this count %d, diagCarry %d\n", tidx, cache[tidx], thisCount, diagCarry)
+			cache[tidx], diagCarry = thisCount, cache[tidx]
+		}
+	}
+	return cache[0]
+}
+
+func numDistinctMem(s string, t string) int {
 	cache := map[[2]int]int{}
 
 	var recur func(int, int) int
