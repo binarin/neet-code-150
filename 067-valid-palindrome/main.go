01247f420fc37deac926ffd5782c83cb6813adad
Alexey Lebedeff <binarin@binarin.info>
2026-03-05 15:09:01 +0100

    valid-palindrome - done

--- 067-valid-palindrome/main.go
+++ 067-valid-palindrome/main.go
@@ -3,10 +3,29 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 func isPalindrome(s string) bool {
-	return false
+	clean := make([]rune, 0, len(s))
+	for _, rune := range s {
+		if unicode.IsNumber(rune) || unicode.IsLower(rune) {
+			clean = append(clean, rune)
+		} else if unicode.IsUpper(rune) {
+			clean = append(clean, unicode.ToLower(rune))
+		}
+	}
+	left, right := 0, len(clean)-1
+	for left <= right {
+		if clean[left] != clean[right] {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
 }
 
 func main() {
