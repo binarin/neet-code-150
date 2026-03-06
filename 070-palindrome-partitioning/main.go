e20dd0a696967c117fd2eb29f4a7eec88d64ee4b
Alexey Lebedeff <binarin@binarin.info>
2026-03-06 18:17:23 +0100

    070-palindrome-partitioning - done

--- 070-palindrome-partitioning/main.go
+++ 070-palindrome-partitioning/main.go
@@ -6,7 +6,45 @@ package main
 import "fmt"
 
 func partition(s string) [][]string {
-	return nil
+	cache := map[string][][]string{}
+
+	var recur func(string) [][]string
+	recur = func(ss string) [][]string {
+		if _, ok := cache[ss]; ok {
+			return cache[ss]
+		}
+		if len(ss) == 0 {
+			return [][]string{{}}
+		}
+		isPalindrome := func(l int) bool {
+			if l == 1 {
+				return true
+			}
+			left, right := 0, l-1
+			for left <= right {
+				if ss[left] != ss[right] {
+					return false
+				}
+				left++
+				right--
+			}
+			return true
+		}
+		result := [][]string{}
+		for pLen := 1; pLen <= len(ss); pLen++ {
+			if isPalindrome(pLen) {
+				prefix := ss[0:pLen]
+				for _, part := range partition(ss[pLen:]) {
+					extended := append([]string{prefix}, part...)
+					result = append(result, extended)
+				}
+			}
+		}
+		cache[ss] = result
+		return result
+	}
+
+	return recur(s)
 }
 
 func main() {
