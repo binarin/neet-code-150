04419dd93734c41425b60ccb4ea6d4fbfb4dcc8f
Alexey Lebedeff <binarin@binarin.info>
2026-03-06 17:27:25 +0100

    069-surrounded-regions - done

--- 069-surrounded-regions/main.go
+++ 069-surrounded-regions/main.go
@@ -6,6 +6,49 @@ package main
 import "fmt"
 
 func solve(board [][]byte) {
+	type pt [2]int
+	queue := []pt{}
+	keep := map[pt]bool{}
+	height, width := len(board), len(board[0])
+
+	enqueue := func(c pt) {
+		if c[0] < 0 || c[0] >= width || c[1] < 0 || c[1] >= height {
+			return
+		}
+		if board[c[1]][c[0]] == 'O' {
+			if _, ok := keep[c]; !ok {
+				queue = append(queue, c)
+				keep[c] = true
+			}
+		}
+	}
+
+	for x := range width {
+		enqueue(pt{x, 0})
+		enqueue(pt{x, height - 1})
+	}
+
+	for y := range height {
+		enqueue(pt{0, y})
+		enqueue(pt{width - 1, y})
+	}
+
+	deltas := [4]pt{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+	for len(queue) > 0 {
+		c := queue[0]
+		queue = queue[1:]
+		for _, d := range deltas {
+			enqueue(pt{c[0] + d[0], c[1] + d[1]})
+		}
+	}
+
+	for y := range height {
+		for x := range width {
+			if board[y][x] == 'O' && !keep[pt{x, y}] {
+				board[y][x] = 'X'
+			}
+		}
+	}
 }
 
 func main() {
