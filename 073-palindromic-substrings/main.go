186419b0300644a4ab19294480b26fc19e9bc70b
Alexey Lebedeff <binarin@binarin.info>
2026-03-08 18:35:11 +0100

    073-palindromic-substrings - done

--- 073-palindromic-substrings/main.go
+++ 073-palindromic-substrings/main.go
@@ -6,9 +6,38 @@ package main
 import "fmt"
 
 func countSubstrings(s string) int {
-	return 0
+	total := 0
+	for center := range len(s) {
+
+		left, right := center, center
+		for s[left] == s[right] {
+			total++
+			left--
+			right++
+			if left < 0 || right >= len(s) {
+				break
+			}
+		}
+
+		left, right = center, center+1
+		for {
+			if left < 0 || right >= len(s) {
+				break
+			}
+			if s[left] != s[right] {
+				break
+			}
+			total++
+			left--
+			right++
+		}
+	}
+
+	// count even len
+	// count odd len
+	return total
 }
 
 func main() {
-	fmt.Println(countSubstrings("abc"))
+	fmt.Println(countSubstrings("aaa"))
 }
