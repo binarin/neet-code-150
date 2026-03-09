ad6fc80d43a68dcc7b5722c44417fc3e7c09173d
Alexey Lebedeff <binarin@binarin.info>
2026-03-09 10:40:48 +0100

    076-word-break - done with trie

--- 076-word-break/main.go
+++ 076-word-break/main.go
@@ -5,8 +5,59 @@ package main
 
 import "fmt"
 
+type Trie struct {
+	IsLeaf  bool
+	Letters [26]*Trie
+}
+
+func charIdx(c byte) int {
+	return int(c - 'a')
+}
+
+func (t *Trie) Add(s string) {
+	for idx := range len(s) {
+		cid := charIdx(s[idx])
+		if t.Letters[cid] == nil {
+			t.Letters[cid] = &Trie{}
+		}
+		t = t.Letters[cid]
+	}
+	t.IsLeaf = true
+}
+
 func wordBreak(s string, wordDict []string) bool {
-	return false
+	breakableAfter := map[int]bool{}
+	trieRoot := &Trie{}
+	for _, word := range wordDict {
+		trieRoot.Add(word)
+	}
+	var recur func(int) bool
+	recur = func(start int) (result bool) {
+		if cache, ok := breakableAfter[start]; ok {
+			return cache
+		}
+		defer (func() { breakableAfter[start] = result })()
+
+		curS, curT := start, trieRoot
+		for {
+			if curS == len(s) {
+				return curT.IsLeaf
+			}
+			if curT.IsLeaf {
+				result := recur(curS)
+				if result {
+					return true
+				}
+			}
+			next := curT.Letters[charIdx(s[curS])]
+			if next == nil {
+				return false
+			}
+			curS++
+			curT = next
+		}
+	}
+	return recur(0)
 }
 
 func main() {
