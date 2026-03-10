108079dc7b33e4e207e3458620cd98ef7c5fae75
Alexey Lebedeff <binarin@binarin.info>
2026-03-10 14:20:33 +0100

    082-find-minimum-in-rotated-sorted-array - done

--- 082-find-minimum-in-rotated-sorted-array/main.go
+++ 082-find-minimum-in-rotated-sorted-array/main.go
@@ -3,10 +3,27 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func findMin(nums []int) int {
-	return 0
+	left, right := 0, len(nums)-1
+	for left < right {
+		if nums[left] <= nums[right] {
+			// sorted sub-array
+			return nums[left]
+		} else {
+			middle := left + (right-left)/2
+			if nums[left] <= nums[middle] {
+				left = middle + 1 // middle is left biased, we’ve already checked it, and need to downsize
+			} else {
+				right = middle
+			}
+		}
+	}
+
+	return nums[left]
 }
 
 func main() {
