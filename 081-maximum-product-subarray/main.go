60f8222062cf24e2c02c844306ac4328f7b1a5c9
Alexey Lebedeff <binarin@binarin.info>
2026-03-10 13:56:20 +0100

    081-maximum-product-subarray - done

--- 081-maximum-product-subarray/main.go
+++ 081-maximum-product-subarray/main.go
@@ -13,20 +13,24 @@ func main() {
 }
 
 func maxProduct(nums []int) int {
-	return maxProductBf(nums)
+	result := math.MinInt
+	idx := len(nums) - 1
+	maxProduct, minProduct := 1, 1
+	for idx >= 0 {
+		maxProduct, minProduct = max(nums[idx], nums[idx]*minProduct, nums[idx]*maxProduct), min(nums[idx], nums[idx]*minProduct, nums[idx]*maxProduct)
+		result = max(maxProduct, result)
+		idx--
+	}
+	return result
 }
 
 func maxProductBf(nums []int) int {
 	result := math.MinInt
 	for i := range nums {
-		minProd, maxProd := 1, 1
+		prod := 1
 		for j := i; j < len(nums); j++ {
-			minProd *= nums[j]
-			maxProd *= nums[j]
-			if maxProd < minProd {
-				minProd, maxProd = maxProd, minProd
-			}
-			result = max(result, minProd, maxProd)
+			prod *= nums[j]
+			result = max(result, prod)
 		}
 	}
 	return result
